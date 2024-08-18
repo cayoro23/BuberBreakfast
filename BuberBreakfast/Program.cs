754c389498f1f7a840f490192e416ce0b06ec52f
Carlos Rodriguez <carmanuel1997@gmail.com>
2024-08-18 18:27:00 -0500

    Dejando la configuracion necesaria para comenzar con el API

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -1,17 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
+{
+    builder.Services.AddControllers();
+}
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
