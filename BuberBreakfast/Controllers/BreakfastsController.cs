be7d126cfe70025c0c6c975d649ca966bad05780
Carlos Rodriguez <carmanuel1997@gmail.com>
2024-08-20 17:28:02 -0500

    Creacion del controlador

---
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -0,0 +1,33 @@
+﻿using BuberBreakfast.Contracts.Breakfast;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberBreakfast.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BreakfastsController : ControllerBase
+{
+    [HttpPost()]
+    public IActionResult CreateBreakfasts(CreateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfasts(Guid id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpsertBreakfasts(Guid id,UpsertBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfasts(Guid id)
+    {
+        return Ok(id);
+    }
+}
