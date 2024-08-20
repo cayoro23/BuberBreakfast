57260c24e3a0767f4a880799604935a14da6f5a5
Carlos Rodriguez <carmanuel1997@gmail.com>
2024-08-20 17:33:57 -0500

    creacion del modelo

---
+++ BuberBreakfast/Models/Breakfasts.cs
@@ -0,0 +1,33 @@
+﻿namespace BuberBreakfast.Models;
+
+public class Breakfasts
+{
+    public Guid Id { get; }
+    public string Name { get; }
+    public string Description { get; }
+    public DateTime StartDateTime { get; }
+    public DateTime EndDateTime { get; }
+    public DateTime LastModifiedDatetime { get; }
+    public List<string> Savory { get; }
+    public List<string> Sweet { get; }
+
+    public Breakfasts(
+        Guid id,
+        string name,
+        string description,
+        DateTime startDateTime,
+        DateTime endDateTime,
+        DateTime lastModifiedDatetime,
+        List<string> savory,
+        List<string> sweet)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        StartDateTime = startDateTime;
+        EndDateTime = endDateTime;
+        LastModifiedDatetime = lastModifiedDatetime;
+        Savory = savory;
+        Sweet = sweet;
+    }
+}
